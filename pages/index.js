9af221b47e4f4586442999dea22d9c31e9ae35d2
Jamie Browning <jamie@jambro.io>
2021-06-08 14:56:21 +0100

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-[#0B1418] min-h-screen flex justify-center items-center">
+      <h1 className="bg-gradient-to-r text-6xl from-green-100 to-green-300  text-transparent bg-clip-text">NextJS | TailwindCSS</h1>
+    </div>
+  );
+}
