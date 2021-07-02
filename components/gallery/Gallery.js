9af62b7fa8ea9d39caf19753aa9989227e84f353
Jamie Browning <jamie@jambro.io>
2021-07-02 13:51:43 +0100

    Add webp images and srcset to gallery

--- components/gallery/Gallery.js
+++ components/gallery/Gallery.js
@@ -1,14 +1,16 @@
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { galleryImages } from '../../data/gallery-images';
+import Image from 'next/image';
 
 export default function ImagesGallery() {
   return galleryImages ? (
     <ImageGallery
-      items={galleryImages}
-      showNav={false}
-      autoPlay={true}
-      showThumbnails={false}
+      items={ galleryImages }
+      showNav={ false }
+      autoPlay={ true }
+      lazyLoad={true}
+      showThumbnails={ false }
     />
   ) : null;
 }
